da528f8e88757ff62952e47ce756a7657a3dcbfb
Ronaldo Costa <devronaldo.costa@gmail.com>
2021-09-01 22:16:35 -0400

    Resumo aula historia do Python

--- fundamentos/helloWorld.py
+++ fundamentos/helloWorld.py
@@ -1,1 +1,18 @@
+"""
+Python é uma linguagem de programação interpretada e de script. Ela executa em uma máquina virtual e o Shell executa
+comandos diretamente.
+
+Python também é uma linguagem multiparadigma: podem ser usados os paradgimas imperativo, funcional e orientado a
+objetos.
+
+Um pouco de história
+
+A linguagem foi criada no final do ano de 1989 por Guido Von Rossun no Instituto de Pesquisa Nacional para Matemática e
+Ciência da Computação, nos Países Baixos. O nome foi inspirado no grupo britânico de comédia Monty Python.
+"""
+
+#Comando print() é usado para escrita no terminal, a mensagem deve vir entre os parentêses e estar entre aspas.
 print("Hello World!")
+
+#Output
+#Hello World!
