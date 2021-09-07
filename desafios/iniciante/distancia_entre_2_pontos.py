118ba87c094cb70308860e8e1568c4f307faa127
Melinne Diniz <61233511+melinnediniz@users.noreply.github.com>
2021-09-07 14:37:48 -0400

    Adiciona comentário

--- desafios/iniciante/distancia_entre_2_pontos.py
+++ desafios/iniciante/distancia_entre_2_pontos.py
@@ -9,7 +9,7 @@ x1 y1 e a segunda linha contém dois valores de ponto flutuante x2 y2.
 -> SAÍDA
 Calcule e imprima o valor da distância segundo a fórmula fornecida, com 4 casas após o ponto decimal.
 '''
-from math import sqrt
+from math import sqrt # utiliza unicamente a função sqrt (raíz quadrada) da biblioteca math
 
 p1 = input() .split()
 x1 = float(p1[0])
