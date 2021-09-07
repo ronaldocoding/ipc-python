49039a72f23f1dc33478a313f6d04f1031d211ca
melinnediniz <melinnediniz@gmail.com>
2021-09-07 14:21:43 -0400

    Adiciona Problema 1011 ao 1017 do URI; Corrige nome do arquivo area

---
+++ desafios/iniciante/distancia_entre_2_pontos.py
@@ -0,0 +1,25 @@
+'''
+Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1,y1)e p2(x2,y2)
+e calcule a distância entre eles, mostrando 4 casas decimais após a vírgula, segundo a fórmula:
+
+<- ENTRADA
+O arquivo de entrada contém duas linhas de dados. A primeira linha contém dois valores de ponto flutuante:
+x1 y1 e a segunda linha contém dois valores de ponto flutuante x2 y2.
+
+-> SAÍDA
+Calcule e imprima o valor da distância segundo a fórmula fornecida, com 4 casas após o ponto decimal.
+'''
+from math import sqrt
+
+p1 = input() .split()
+x1 = float(p1[0])
+y1 = float(p1[1])
+
+p2 = input() .split()
+x2 = float(p2[0])
+y2 = float(p2[1])
+
+dist = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+print("{:.4f}".format(dist))
+
